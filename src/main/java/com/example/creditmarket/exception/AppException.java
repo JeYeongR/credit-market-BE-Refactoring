5bb7c341f57e1d5c3e2c4464b1e5e8f29f1792bc
JeYeongR <wpdud2003@gmail.com>
2023-03-02 19:01:42 +0900

    refactor: #4 AppException 생성자 수정

--- src/main/java/com/example/creditmarket/exception/AppException.java
+++ src/main/java/com/example/creditmarket/exception/AppException.java
@@ -1,11 +1,14 @@
 package com.example.creditmarket.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
-public class AppException extends RuntimeException{
+public class AppException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
+
+    public AppException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
 }
