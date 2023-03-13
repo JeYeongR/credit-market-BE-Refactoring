720cb2d9eed6f9c33055c9b2a858f1631c7d88ba
JeYeongR <wpdud2003@gmail.com>
2023-03-13 16:16:45 +0900

    refactor: #24 Service 로직 수정

--- src/main/java/com/example/creditmarket/service/OrderService.java
+++ src/main/java/com/example/creditmarket/service/OrderService.java
@@ -1,9 +1,9 @@
 package com.example.creditmarket.service;
 
-import com.example.creditmarket.dto.request.OrderSaveRequestDTO;
+import com.example.creditmarket.dto.request.OrderAddListRequestDTO;
 import com.example.creditmarket.dto.response.OrderListResponseDTO;
-import com.example.creditmarket.dto.response.ProductDetailResponseDTO;
-import com.example.creditmarket.dto.response.RecommendResponseDTO;
+import com.example.creditmarket.dto.response.ProdDetailResponseDTO;
+import com.example.creditmarket.dto.response.RecListResponseDTO;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -12,7 +12,7 @@ public interface OrderService {
 
     ProductDetailResponseDTO getProductDetail(String productId, HttpServletRequest request);
 
-    String buyProduct(OrderSaveRequestDTO requestDTO, String userEmail);
+    String buyProduct(OrderAddListRequestDTO orderAddListRequestDTO, String userEmail);
 
     List<RecommendResponseDTO> recommendList(HttpServletRequest request);
 
